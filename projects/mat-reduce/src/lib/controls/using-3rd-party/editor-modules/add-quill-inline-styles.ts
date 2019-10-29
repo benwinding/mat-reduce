0bf4f05cac025ebd4aec8440a4baff807765f11b
Johann Munoz <johannmunoz.dev@gmail.com>
2019-10-29 16:03:26 +1030

    Fix alignment img

--- projects/mat-reduce/src/lib/controls/using-3rd-party/editor-modules/add-quill-inline-styles.ts
+++ projects/mat-reduce/src/lib/controls/using-3rd-party/editor-modules/add-quill-inline-styles.ts
@@ -39,4 +39,31 @@ export function AddQuillInlineStyles(Quill: any) {
   const SizeStyle = Quill.import('attributors/style/size');
   Quill.register(SizeStyle, true);
   // create new Quill instance after...
+
+  const BaseImageFormat = Quill.import('formats/image');
+  const ImageFormatAttributesList = ['alt', 'height', 'width', 'style'];
+
+  class ImageFormat extends BaseImageFormat {
+    static formats(domNode) {
+      return ImageFormatAttributesList.reduce((formats, attribute) => {
+        if (domNode.hasAttribute(attribute)) {
+          formats[attribute] = domNode.getAttribute(attribute);
+        }
+        return formats;
+      }, {});
+    }
+    format(name, value) {
+      if (ImageFormatAttributesList.indexOf(name) > -1) {
+        if (value) {
+          this.domNode.setAttribute(name, value);
+        } else {
+          this.domNode.removeAttribute(name);
+        }
+      } else {
+        super.format(name, value);
+      }
+    }
+  }
+
+  Quill.register(ImageFormat, true);
 }

dcd29e99ce2030d475d621c165526693d32945fe
Ben Winding <hello@benwinding.com>
2019-11-11 18:12:36 +1030

    form-date tests

---
+++ projects/mat-reduce-demo/src/app/form-mocks/test-date.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  template: `
+    <h1>form-text Testing</h1>
+
+    <form-toggle
+      [formControl]="formControlEnabled"
+      placeholder="Form Enabled"
+    ></form-toggle>
+
+    <form-date
+      *ngIf="loaded"
+      [formControl]="AfterDateControl"
+      placeholder="AfterDate"
+    >
+    </form-date>
+    <form-date
+      *ngIf="loaded"
+      [formControl]="testControl"
+      [AfterDate]="AfterDateControl?.value"
+      placeholder="Must be after [AfterDate]!"
+    >
+    </form-date>
+
+    <h4>Value</h4>
+    <pre *ngIf="testControl as c">
+    {{ { value: c.value, valid: c.valid } | json}}
+  </pre
+    >
+    <div style="width: 400px;">
+      <form-date
+        *ngIf="loaded"
+        [formControl]="testControlRequired"
+        placeholder="Select date! (Required)"
+      >
+      </form-date>
+    </div>
+    <h4>Value</h4>
+    <pre *ngIf="testControlRequired as c">
+    {{ { value: c.value, valid: c.valid } | json}}
+  </pre
+    >
+  `
+})
+export class TestDateComponent {
+  formControlEnabled = new FormControl(true);
+  loaded = false;
+
+  testControl = new FormControl(new Date());
+  AfterDateControl = new FormControl(new Date());
+  testControlRequired = new FormControl(null, Validators.required);
+
+  constructor() {
+    this.formControlEnabled.valueChanges.subscribe(isEnabled => {
+      if (isEnabled) {
+        this.testControl.enable();
+        this.testControlRequired.enable();
+        setTimeout(() => {
+          this.loaded = true;
+        }, 1000);
+      } else {
+        this.testControlRequired.disable();
+        this.testControl.disable();
+      }
+    });
+  }
+}

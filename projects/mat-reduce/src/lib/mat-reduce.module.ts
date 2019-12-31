71f8406bb5dc75fba997a727953cdd4e99ac6173
Ben Winding <ben.winding@gmail.com>
2019-12-31 12:21:08 +1030

    Core module included

--- projects/mat-reduce/src/lib/mat-reduce.module.ts
+++ projects/mat-reduce/src/lib/mat-reduce.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatReduceQuillEditorModule } from './quill-editor/quill-editor.module';
-// import { MatReduceCoreModule } from './from-core';
+import { MatReduceCoreModule } from './from-core';
 
 const exportedModules = [
   CommonModule,
-  // MatReduceCoreModule,
+  MatReduceCoreModule,
   MatReduceQuillEditorModule
 ];
 

053d6ac9f506cd92ef6e085552edf80d4f170274
Ben Winding <ben.winding@gmail.com>
2019-12-31 12:51:11 +1030

    Add missing exports

--- projects/mat-reduce/src/public_api.ts
+++ projects/mat-reduce/src/public_api.ts
@@ -1,5 +1,23 @@
+import {
+  FormBase,
+  FormGroupTypeSafe,
+  FormArrayTypeSafe,
+  FormBuilderTypedService,
+  Tag,
+  TagObject
+} from 'mat-reduce-core';
+
 /*
  * Public API Surface of mat-reduce
  */
 
+// export * from 'mat-reduce-core';
 export * from './lib/mat-reduce.module';
+export {
+  FormBase,
+  FormGroupTypeSafe,
+  FormArrayTypeSafe,
+  FormBuilderTypedService,
+  Tag,
+  TagObject
+};
